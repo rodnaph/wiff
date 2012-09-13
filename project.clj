e9c48942b0ae6b576443e6b55805c8d71ed6854a
rod <rod@pu-gh.com>
2012-09-14 00:02:01 +0100

    tweaked styles, and escaped html

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  [compojure "1.1.1"]
                  [ring/ring-jetty-adapter "1.1.2"]
                  [ring/ring-devel "1.1.3"]
+                 [jiff "0.0.2"]
                  [hiccup "1.0.1"]]
   :plugins [[lein-ring "0.7.2"]]
   :main wiff.core)
--- src/wiff/pages.clj
+++ src/wiff/pages.clj
@@ -1,8 +1,11 @@
 
 (ns wiff.pages
   (:use hiccup.core
-        hiccup.form)
-  (:require (hiccup [page :as page])))
+        hiccup.form
+        jiff.vcs)
+  (:require jiff.svn
+            (hiccup [page :as page])
+            [clojure.string :as string]))
 
 (defn- url-row [text id value]
   [:div.control-group
@@ -16,6 +19,34 @@
     (url-row "URL 2" "url2" url2)
     (submit-button {:class "btn btn-primary"} "Compare")))
 
+(defn- line-type [line]
+  (cond
+    (re-matches #"^\+.*" line) :pre.add
+    (re-matches #"^-.*" line) :pre.remove
+    :else :pre))
+
+(defn- html-escape [line]
+  (-> line
+      (string/replace #"<" "&lt;")
+      (string/replace #">" "&gt;")))
+
+(defn- to-line [line]
+  [(line-type line) (html-escape line)])
+
+(defn- to-file-diff [file]
+  [:div
+    [:h3 (:path file)]
+    [:div.code
+      (map to-line (:lines file))]])
+
+(defn- diff-view [{:keys [url1 url2]}]
+  (let [files (jiff-seq {:vcs :svn
+                         :from url1
+                         :to url2})]
+    [:div.row
+      [:div.span12
+        (map to-file-diff files)]]))
+
 ;; Public
 ;; ------
 
@@ -23,7 +54,8 @@
   (html
     [:head
       [:title "Wiff!"]
-      (page/include-css "/assets/bootstrap/css/bootstrap.min.css")]
+      (page/include-css "/assets/bootstrap/css/bootstrap.min.css")
+      (page/include-css "/assets/css/default.css")]
     [:body
       [:div.container
         [:div.row
@@ -31,5 +63,6 @@
             [:h1 "Wiff Web Differ"]]]
         [:div.row
           [:div.span12
-            (url-form params)]]]]))
+            (url-form params)]]
+       (diff-view params)]]))
 
